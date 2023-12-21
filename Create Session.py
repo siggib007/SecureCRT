42a892243ab3f88e875590a1ef629e4ecbb60ebc
Siggi@Reba <siggib007@users.noreply.github.com>
2023-12-21 22:10:00 +0000

    wip

--- Create Session.py
+++ Create Session.py
@@ -1,33 +1,50 @@
 # $language = "Python"
 # $interface = "1.0"
 
-def createSession(strSessionPath):
+def createSession(dictSession):
+  if "Path" not in dictSession:
+    return "No Path"
+  if dictSession["Path"] == "":
+    return "No Path"
+
+  if "HostName" in dictSession:
+    strHostName = dictSession["HostName"]
+  else:
+    strHostName = ""
+  if "User" in dictSession:
+    strUser = dictSession["User"]
+  else:
+    strUser = ""
+  if "Cred" in dictSession:
+    strCred = dictSession["Cred"]
+  else:
+    strCred = ""
+  if "FW" in dictSession:
+    strFW = dictSession["FW"]
+  else:
+    strFW = ""
+  if "Port" in dictSession:
+    strPort = dictSession["Port"]
+  else:
+    strPort = 22
+
   try:
-    objSession = crt.OpenSessionConfiguration(strSessionPath)
+    objSession = crt.OpenSessionConfiguration(dictSession["Path"])
   except Exception as err:
     objSession = crt.OpenSessionConfiguration()
-  objSession.SetOption("Hostname","test5.supergeek.is")
-  objSession.SetOption("Username","siggib")
-  # objSession.SetOption("[SSH2] Port",22)
-  #objSession.SetOption("Session Password Saved", 1)
-  #objSession.SetOption("Password","MyTesting123!")
-  objSession.Save(strSessionPath)
 
-def readSession(strSessionPath):
-  objSession = crt.OpenSessionConfiguration(strSessionPath)
-  strHostname = objSession.GetOption("Hostname")
-  crt.Dialog.MessageBox("Hostname:\r\n{0}".format(strHostname))
-  strPassword = objSession.GetOption("Password")
-  crt.Dialog.MessageBox("Password:\r\n{0}".format(strPassword))
+  objSession.SetOption("Hostname",strHostName)
+  objSession.SetOption("Username",strUser)
+  objSession.SetOption("Credential Title",strCred)
+  objSession.SetOption("Firewall Name",strFW)
+  objSession.SetOption("[SSH2] Port",strPort)
+  objSession.Save(dictSession["Path"])
+  return "Success"
 
-def ListSession():
-  objSessions = crt.Session
-  if isinstance(objSessions,list):
-    crt.Dialog.MessageBox("Sessions object is a list")
-  else:
-    crt.Dialog.MessageBox("Sessions object is not a list")
-
-#ListSession()
-createSession("test/test5")
-#readSession("test/Second Test")
+dictTemp = {}
+dictTemp["Path"] = "mytest"
+dictTemp["HostName"] = "mytest.supergeek.is"
+dictTemp["Cred"] = "siggi-key"
+dictTemp["FW"] = "Session:Nanitor\Nanitor Jump"
+createSession(dictTemp)
 crt.Dialog.MessageBox("Done")
